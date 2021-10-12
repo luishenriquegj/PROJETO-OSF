9f9b9bdc8179c451b9b10891e93bb8ea5dca2e0b
luishenriquegj <lhjuca@gmail.com>
2021-10-12 06:58:11 -0300

    css att
    formatted the code and added more css (responsive, card borders, background colors)

--- Pokelist/src/Navbar.js
+++ Pokelist/src/Navbar.js
@@ -2,27 +2,11 @@ const Navbar = () =>{
     return (
         <nav className = "navbar">
             <img className="siteIcon" src = '.\Assets\images\icon.png'></img>
-            <h1 style = {{
-                color : "#f1356d",
-                padding : '4px',
-                backgroundColor: 'white',
-                borderRadius : '28px',
-                borderColor : '#f1356d',
-                border : '4px  solid',
-
-            }}>Pokelist</h1>
+            <h1 className ='siteTitle'>Pokelist</h1>
             <div className ="links"> 
-                <a href ="\" style ={{
-                    color : "white",
-                    backgroundColor: '#f1356d',
-                    borderRadius : '28px',
-                   
-                } } className ="Button">Home </a>       
-                <a href ="\create" style ={{
-                    color : "white",
-                    backgroundColor: '#f1356d',
-                    borderRadius : '28px',
-                }}className ="Button">Add new Pokemons </a>  
+                <a href ="\"  className ="Button">Home </a>   
+                &nbsp;&nbsp;    
+                <a href ="\create" className ="Button">Add new Pokemons </a>  
             </div>
         </nav>
 
